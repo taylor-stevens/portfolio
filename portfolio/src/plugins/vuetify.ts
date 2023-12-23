5b761f9a962f1274381a48ef3550555a56170804
Taylor Stevens <taylorstevens14562@gmail.com>
2023-12-22 20:26:00 -0500

    using vue

---
+++ portfolio/src/plugins/vuetify.ts
@@ -0,0 +1,36 @@
+/**
+ * plugins/vuetify.ts
+ *
+ * Framework documentation: https://vuetifyjs.com`
+ */
+
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+
+import '@fortawesome/fontawesome-free/css/all.css' // Ensure your project is capable of handling css files
+import { aliases, fa } from 'vuetify/iconsets/fa'
+
+// Composables
+import { createVuetify } from 'vuetify'
+
+// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+export default createVuetify({
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#1867C0',
+          secondary: '#5CBBF6',
+        },
+      },
+    },
+  },
+  icons: {
+    defaultSet: 'fa',
+    aliases,
+    sets: {
+      fa,
+    },
+  },
+})
