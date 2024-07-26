864639d57bfadb456429855b0498b6908c845ad0
Taylor Stevens <taylorstevens14562@gmail.com>
2024-07-26 13:19:12 -0400

    updating experiences

---
+++ src/components/Experience.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-card>
+        <v-card-item v-slot:prepend>
+            <div class="text-overline mb-1">
+                {{ experience.jobStartDate + ' - ' + experience.jobEndDate }}
+            </div>
+            <div class="text-h6">
+                {{ experience.jobTitle }}
+            </div>
+            <v-card-subtitle>{{ experience.jobCompany + ' in ' + experience.jobLocation }}</v-card-subtitle>
+        </v-card-item>
+        <v-card-text v-for="description in experience.jobDescription" style="padding-top: 0; padding-bottom: 2%;">
+            {{ description }}
+        </v-card-text>
+        <v-card :v-if="experience.previouslyHeldPosition">
+            
+        </v-card>
+    </v-card>
+</template>
+
+<script>
+    export default {
+    data: () => ({
+    }),
+    props: {
+        experience: {
+            type: Object,
+            default: {
+                "jobTitle": "Job Title",
+                "jobLocation": "Location, Place, Country",
+                "jobCompany": "Job Company",
+                "jobStartDate": "Month 01, Year",
+                "jobEndDate": "Month 30, Year",
+                "remote": false,
+                "inPerson": true,
+                "jobDescription": [
+                    "A brief discription of this job"
+                ],
+            }
+        },
+    }
+}
+</script>
--- src/components/Experiences.vue
+++ src/components/Experiences.vue
@@ -1,32 +1,24 @@
 <template>
     <v-container>
         <v-row>
-            <v-col cols="12" md="6" v-for="experience in experiences">
-                <v-card>
-                    <v-card-item v-slot:prepend>
-                        <div class="text-overline mb-1">
-                            {{ experience.jobStartDate + ' - ' + experience.jobEndDate }}
-                        </div>
-                        <div class="text-h6">
-                            {{ experience.jobTitle }}
-                        </div>
-                        <v-card-subtitle>{{ experience.jobCompany + ' in ' + experience.jobLocation }}</v-card-subtitle>
-                    </v-card-item>
-                    <v-card-text v-for="description in experience.jobDescription" style="padding-top: 0; padding-bottom: 2%;">
-                        {{ description }}
-                    </v-card-text>
-                </v-card>
+            <v-col cols="12" v-for="experience in experiences">
+                <Experience :experience="experience"/>
+                <Experience v-if="experience.previouslyHeldPosition" :experience="experience.previouslyHeldPosition"/>                    
             </v-col>
         </v-row>
     </v-container>
 </template>
 
 <script>
+    import Experience from './Experience.vue';
     import experiences from './data/experiences.json'
 
     export default {
-    data: () => ({
-        experiences: experiences
-    }),
+        data: () => ({
+            experiences: experiences
+        }),
+        components: {
+            Experience
+        }
     }
 </script>
